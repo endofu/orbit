8adc0d19d3299b91147e9ef594feb0531c5f2630
endofu <endofu@gmail.com>
2012-11-30 12:38:01 +0000

    animation setup

--- client/js/orbit.js
+++ client/js/orbit.js
@@ -1,8 +1,20 @@
+
 $(function() {
-var socket = io.connect('http://localhost');
-socket.on('bang', function (data) {
+orbit = window['orbit'] = {}
+orbit.draw = function() {
+    requestAnimationFrame(orbit.draw);
+    $('body').css("background-color", "yellow");
+}
+
+
+orbit.socket = io.connect('http://localhost');
+orbit.socket.on('bang', function (data) {
     console.log(data);
+    orbit.draw();
     //socket.emit('my other event', { my: 'data' });
 });
 })
+
+
+
 
