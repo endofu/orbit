c7f6ab377b236f392b2b222f025fa773d6c5f9fd
endofu <endofu@gmail.com>
2012-11-30 13:27:39 +0000

    pocket io work

--- client/js/orbit.js
+++ client/js/orbit.js
@@ -1,18 +1,24 @@
 
 $(function() {
-orbit = window['orbit'] = {}
-orbit.draw = function() {
-    requestAnimationFrame(orbit.draw);
-    $('body').css("background-color", "yellow");
-}
+    orbit = window['orbit'] = {}
+    orbit.debug = true;
+    orbit.draw = function() {
+        requestAnimationFrame(orbit.draw);
+        $('body').css("background-color", "yellow");
+    }
 
-
-orbit.socket = io.connect('http://localhost');
-orbit.socket.on('bang', function (data) {
-    console.log(data);
-    orbit.draw();
-    //socket.emit('my other event', { my: 'data' });
-});
+    if (!orbit.debug)
+    {
+        orbit.socket = io.connect('http://localhost');
+        orbit.socket.on('handshake', function (data) {
+            console.log(data);
+            orbit.draw();
+            //socket.emit('my other event', { my: 'data' });
+        });
+        orbit.socket.on('newsound', function (data) {
+            console.log(data);
+        });
+    }
 })
 
 
